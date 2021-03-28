bad1204e2022e44eac94725e4c4acac12e38e6c4
LarsRosenkilde <50791042+LarsRosenkilde@users.noreply.github.com>
2021-03-28 18:37:20 +0200

    Start

---
+++ download.py
@@ -0,0 +1,38 @@
+from __future__ import unicode_literals
+import os
+import youtube_dl
+
+
+class YoutubeDownloader(object):
+    """ Simple YouTube MP3 Downloader """
+    default_savepath = os.path.join(os.path.expanduser("~", "Downloads"))
+    
+    def __init__(self, save_path=default_savepath, no_playlist=True, url):
+        self.save_path = save_path
+        self.no_playlist = no_playlist
+        self.url = url
+    
+    
+    def opts(self):
+        return {
+            "verbose": True,
+            "fixup"  : "detect_or_warn",
+            "format" : "bestaudio/best",
+            "postprocessors" : [(
+                "key": "FFmpegExtractAudio",
+                "preferedcodec"  : "mp3",
+                "preferedquality": "1411"
+            )]
+            "extractaudio": True,
+            "outtmpl"     : save_path + "/%(title)s.%(ext)s",
+            "noplaylist"  : self.no_playlist
+        }
+        
+    def downlaod(self):
+        download_object = youtube_dl.YoutubeDL(self.opts())
+        download_object([self.url])
+
+
+if __name__ == "__main__":
+    url = input(r"Enter url for song: ")
+    download = YoutubeDownloader(url)
